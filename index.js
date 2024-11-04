126098b2f93a1932671a08096676f4c3d562a50e
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-11-04 15:18:57 +0300

    setting up event listener for DOMContentLoaded and overwriting text

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener(`DOMContentLoaded`, ()=>{
+    document.getElementById("text").innerHTML = "This is really cool!"
+    /*if (text.innerHTML === `JavaScript is so cool. It lets me add text to my page programmatically.`){
+        text.innerHTML === `This is really cool!`
+    } else {
+        text.innerHTML === `JavaScript is so cool. It lets me add text to my page programmatically.`
+    }*/
+    text.target.textContent = (`This is really cool!`);
+})
